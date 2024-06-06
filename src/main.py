186dedaa0f739fcd12e0bd051f5b012297d22e8a
Robert Rabbe <rora00001@stud.uni-saarland.de>
2024-06-06 14:46:11 +0200

    Changed a name from file_name to file_path to avoid ambiguity

--- src/main.py
+++ src/main.py
@@ -7,37 +7,37 @@ from model.run_request import get_best_answer
 
 def main():
     if len(sys.argv) < 2:
-        print("Usage: python script.py <file_name> [sheet_name] [question_column] [answer_column]")
+        print("Usage: python script.py <file_path> [sheet_name] [question_column] [answer_column]")
         return
 
-    file_name = sys.argv[1]
+    file_path = sys.argv[1]
 
-    if file_name.endswith('.pdf'):
-        questions = extract_questions_from_pdf(file_name)
+    if file_path.endswith('.pdf'):
+        questions = extract_questions_from_pdf(file_path)
         answers = []
         for q in questions:
             answers.append(get_best_answer(q))
 
-    elif file_name.endswith('.docx'):
+    elif file_path.endswith('.docx'):
         questions = extract_questions_docx
         answers = []
         for q in questions:
             answers.append(get_best_answer(q))
 
-    elif file_name.endswith('.xlsx'):
+    elif file_path.endswith('.xlsx'):
         if len(sys.argv) != 5:
-            print("Usage for Excel files: python script.py <file_name> <sheet_name> <question_column> <answer_column>")
+            print("Usage for Excel files: python script.py <file_path> <sheet_name> <question_column> <answer_column>")
             return
         sheet_name = sys.argv[2]
         question_column = sys.argv[3]
         answer_column = sys.argv[4]
-        questions = fill_out_xlsx(file_name, sheet_name, question_column, answer_column)
+        questions = fill_out_xlsx(file_path, sheet_name, question_column, answer_column)
         answers = []
         for q in questions:
             answers.append(get_best_answer(q))
 
-    elif file_name.endswith('.png'):
-        questions = get_questions_from_image(file_name)
+    elif file_path.endswith('.png'):
+        questions = get_questions_from_image(file_path)
         answers = []
         for q in questions:
             answers.append(get_best_answer(q))
